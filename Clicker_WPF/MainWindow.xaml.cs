a80076eacca6611e1b56771f115b54d297cd629e
MorOlesya <dl9morozova@gmail.com>
2025-11-08 13:01:56 +0300

    Logic Clicker

--- Clicker_WPF/MainWindow.xaml.cs
+++ Clicker_WPF/MainWindow.xaml.cs
@@ -1,18 +1,10 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Clicker_WPF
 {
     public partial class MainWindow : Window
     {
+        private int _counter = 0;
         public MainWindow()
         {
             InitializeComponent();
@@ -20,7 +12,8 @@ namespace Clicker_WPF
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            _counter++;
+            CounterText.Text = $"Счётчик: {_counter}";
         }
     }
 }
